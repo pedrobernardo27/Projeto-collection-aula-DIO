89aabb3508cb103b300fe7a38b13c9abe3eec4c1
pedrobernardo27 <104027175+pedrobernardo27@users.noreply.github.com>
2022-06-09 15:54:10 -0300

    criação da classe ArrayDouble Teste

---
+++ src/main/kotlin/one/digitalinnovation/collections/testeDoubleArray.kt
@@ -0,0 +1,28 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = DoubleArray(3)
+
+    salarios[0] = 1000.0
+    salarios[1] = 2000.0
+    salarios[2] = 4000.0
+
+    salarios.forEach {
+        println(it)
+    }
+    println("------------------------")
+
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    println("------------------------")
+    salarios.forEach {
+        println(it)
+    }
+    println("------------------------")
+    val salarios2 = doubleArrayOf(1500.0, 1250.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach {
+        println(it)
+    }
+}
